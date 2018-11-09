3290e1cbe0f90b4e7dcfb88cff1ed47efb9f4fbf
DIR\peteris.caurs <peteris.caurs@accenture.com>
2018-11-09 16:03:44 +0200

    Updates

---
+++ precache-manifest.2676975db6d3721393b7e4f2a3644188.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/google-it/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "bdddcaa2537af0763e20",
+    "url": "/google-it/static/css/main.9fff7877.chunk.css"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/google-it/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "9d8ca4c37a175de280b7",
+    "url": "/google-it/static/js/1.9d8ca4c3.chunk.js"
+  },
+  {
+    "revision": "f2a5b50ae736fd991448",
+    "url": "/google-it/static/js/runtime~main.f2a5b50a.js"
+  },
+  {
+    "revision": "c83b84dad8f9d209f9a7d0ff50404aab",
+    "url": "/google-it/static/media/wheat.c83b84da.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/google-it/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/google-it/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/google-it/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/google-it/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/google-it/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/google-it/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/google-it/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/google-it/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/google-it/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/google-it/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/google-it/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "bdddcaa2537af0763e20",
+    "url": "/google-it/static/js/main.bdddcaa2.chunk.js"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/google-it/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/google-it/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/google-it/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/google-it/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/google-it/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/google-it/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/google-it/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/google-it/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/google-it/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/google-it/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/google-it/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/google-it/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/google-it/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/google-it/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "9d8ca4c37a175de280b7",
+    "url": "/google-it/static/css/1.484c07a3.chunk.css"
+  },
+  {
+    "revision": "57883934a97cfe198274095f10141ce2",
+    "url": "/google-it/index.html"
+  }
+];
