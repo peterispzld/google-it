e71924205f0c8d899880011cabf3341885de13a8
DIR\peteris.caurs <peteris.caurs@accenture.com>
2018-11-09 16:00:28 +0200

    Setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import SearchQueryStore from './models/SearchQueryStore';
+import { getSnapshot, destroy, onSnapshot } from 'mobx-state-tree';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+const localStorageKey = "google-it";
+
+const initialState = localStorage.getItem(localStorageKey)
+    ? JSON.parse(localStorage.getItem(localStorageKey))
+    : { searchQueries: [] }
+
+let store;
+let snapshotListener;
+function createSearchQueryStore(snapshot) {
+    if (snapshotListener) snapshotListener()
+    if (store) destroy(store)
+
+    store = SearchQueryStore.create(snapshot)
+
+    snapshotListener = onSnapshot(store, snapshot =>
+        localStorage.setItem(localStorageKey, JSON.stringify(snapshot))
+    );
+
+    return store
+}
+
+function renderApp(App, store) {
+    ReactDOM.render(<App store={store} />, document.getElementById("root"))
+}
+
+renderApp(App, createSearchQueryStore(initialState))
+
+if (module.hot) {
+    module.hot.accept(["./models/SearchQueryStore"], () => {
+        renderApp(App, createSearchQueryStore(getSnapshot(store)))
+    })
+    module.hot.accept(["./components/App"], () => {
+        renderApp(App, store)
+    })
+}
+
 serviceWorker.unregister();
